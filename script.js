4c60a16341423a410b69cb1d2d203357148de9b0
Planteig <Planteig17@hotmail.com>
2023-10-04 16:19:10 +0200

    Opgave 3

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function diceRoll(amountOfRolls) {
     for(let i = 0; i < amountOfRolls; i++) {
         // Create the random roll
         let randomDiceRoll = Math.floor(Math.random() * 6 )+ 1;
-
+        // Check if the dice landed on 6
         if (randomDiceRoll === 6 ) {
             console.log("You just hit 6!")
         } else  {
@@ -49,8 +49,49 @@ function diceRoll(amountOfRolls) {
         console.log("Jackpot 🎉")
     }
 }
-// Eksempler med brug af funktionen
+// Examples of function
 diceRoll(3)
 
+// Opgave 3
 
+//A sentiment analyzer is some functionality that figures out how positive/negative a sentence is.
+// Fx the sentence "I am mega super awesome happy" Should have a high score The sentence
+// "I hate doing boring stuff" should have a low score.
+// Create a function that takes a string as a parameter.
+// Calling the function will return an object with score, positiveWords and negativeWords.
+// You decide how the score should be implemented and what words are negative and positive.
+// Here is an example of using the function:
+
+function getSentimentScore(sentence) {
+    // What words is positive and negative
+    const listOfPositiveWords = ["happy","great","awesome","super","fantastic","love"];
+    const listOfNegativeWords = ["sad","awful","unhappy","hate","annoying","bad"];
+    // Make the return object
+    const resultOfAnalysis = {
+        score: 0,
+        positiveWords: [],
+        negativeWords: []
+    }
+
+    //Make the input to lowercase (caseInsensitive) and split each word
+    const inputSentence = sentence.toLowerCase().split(" ");
+    // Check to see if the sentence includes pos or neg words and calculate score
+    for (let word of inputSentence) {
+        if (listOfPositiveWords.includes(word)) {
+            resultOfAnalysis.score += 1;
+            resultOfAnalysis.positiveWords.push(word);
+        } else if (listOfNegativeWords.includes(word)) {
+            resultOfAnalysis.score -= 1;
+            resultOfAnalysis.negativeWords.push(word);
+        }
+    }
+    return resultOfAnalysis
+}
+
+// Examples of function
+const sentimentScoreObjectTest1 = getSentimentScore('I am mega super awesome happy');
+const sentimentScoreObjectTest2 = getSentimentScore('I Am SaD and vEry UNHaPPY');
+
+console.log(sentimentScoreObjectTest1);
+console.log(sentimentScoreObjectTest2);
 
